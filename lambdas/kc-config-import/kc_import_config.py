ec9c41c37d9b2f0c63e9cb5bf38af2c5dde937fe
Weston Boone <wboone@navexglobal.com>
2019-12-24 11:17:02 -0800

    Config functionally imports on deploy
    ...but currently has a real dumb chicken/egg situation on uploading to
    the s3 bucket :/ Basically the cfn template creates the bucket and the
    task reads it into a docker volume, but the pipeline is the one to
    actually upload the files so that is an error on first run and for
    future deploys the config import would lag a deployment

--- lambdas/kc-config-import/kc_import_config.py
+++ lambdas/kc-config-import/kc_import_config.py
@@ -24,6 +24,7 @@ def get_task_logs_location(task_definition_arn: str, task_arn: str) -> Tuple[str
 def run_task(cluster: str, task_subnets: List[str], task_definition: str) -> dict:
     # public ip is needed for fargate so that it can pull the container image.
     # if we set up a nat gateway/instance this can just be set to disabled either way.
+    logger.info(f'Starting task {task_definition} in cluster {cluster}...')
     tasks = ecs_client.run_task(
         cluster              = cluster,
         launchType           = 'FARGATE',
@@ -39,6 +40,7 @@ def run_task(cluster: str, task_subnets: List[str], task_definition: str) -> dic
     return tasks['tasks'][0]
 
 def stop_task(cluster: str, task_arn: str, reason: str):
+    logger.info(f'Stopping task {task_arn} in cluster {cluster} becase: {reason}')
     ecs_client.stop_task(cluster=cluster, task=task_arn, reason=reason)
 
 def get_timestamp_now() -> int:
@@ -46,19 +48,19 @@ def get_timestamp_now() -> int:
 
 def kc_finished_starting(group: str, stream: str) -> bool:
     regex_match = r".*Keycloak \d\.\d\.\d \(WildFly Core \d+\.\d\.\d\.Final\) started"
+    logger.info(f"Checking log group {group} and stream {stream} for logs that match 'started'")
     paginator = logs_client.get_paginator("filter_log_events")
     messages = []
     for logs in paginator.paginate(logGroupName=group, logStreamNames=[stream], filterPattern='started'):
         messages += [ event['message'] for event in logs['events'] if re.match(regex_match, event['message']) ]
     return len(messages) >= 3
 
-def task_running(cluster: str, task_arn: str) -> bool:
+def get_task_status(cluster: str, task_arn: str) -> str:
     tasks = ecs_client.describe_tasks(cluster=cluster, tasks=[task_arn])['tasks']
-    return tasks[0]['containers'][0]['lastStatus'] != 'STOPPED'
+    return tasks[0]['lastStatus']
 
-def taken_too_long(start_time: int) -> bool:
-    ten_minutes = 600000
-    return (get_timestamp_now() - start_time) > ten_minutes
+def taken_too_long(start_time: int, ttl_minutes: int = 5) -> bool:
+    return (get_timestamp_now() - start_time) > (ttl_minutes * 60 * 1000)
 
 @helper.create
 @helper.update
@@ -69,7 +71,7 @@ def create_update(event, context):
     task_subnets = event['ResourceProperties']['TaskSubnets']
     start_time = get_timestamp_now()
     task = run_task(cluster, task_subnets, task_definition)
-    log_group, log_stream = task_definition(task_definition, task['taskArn'])
+    log_group, log_stream = get_task_logs_location(task_definition, task['taskArn'])
     helper.Data.update({
         'LogGroup': log_group,
         'LogStream': log_stream, 
@@ -87,9 +89,15 @@ def poll_create_update(event, context):
     cluster = event['ResourceProperties']['Cluster']
     task_arn = event['CrHelperData']['TaskArn']
     start_time = event['CrHelperData']['TaskStartTime']
-    if not task_running(cluster, task_arn):
+    task_status = get_task_status(cluster, task_arn)
+    task_timed_out = taken_too_long(start_time)
+    if task_status  == 'STOPPED':
         raise RuntimeError(f"{task_arn} was found in unexpected stop state. Check {log_group}/{log_stream} for logs")
-    if not kc_finished_starting(log_group, log_stream) and taken_too_long(start_time):
+    # might be able to put a max runtime in task def and not need the task_timed_out check here
+    elif task_status != 'RUNNING' and not task_timed_out:
+        logger.info(f"{task_arn} status {task_status} not in stable state; checking again later")
+        return
+    if task_status == 'RUNNING' and not kc_finished_starting(log_group, log_stream) and task_timed_out:
         stop_task(cluster, task_arn, "Import took longer than we want to wait")
         raise RuntimeError(f"KC Import config task ({task_arn}) still not done starting and we are done waiting")
     stop_task(cluster, task_arn, "Sucessfully imported config to kc")
