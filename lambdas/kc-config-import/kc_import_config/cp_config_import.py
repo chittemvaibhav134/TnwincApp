2ea37e8184254156d8618727568e19c8a42db077
Weston Boone <wboone@navexglobal.com>
2019-12-31 10:50:31 -0800

    Adds pending import condition to cp import lambda
    During a refactor i apparentlt nuked handling this condition :/ the
    result was the default fall through logic would report a success as long
    as the task was RUNNING for less than 5 minutes when codepipeline
    checked again

--- lambdas/kc-config-import/kc_import_config/cp_config_import.py
+++ lambdas/kc-config-import/kc_import_config/cp_config_import.py
@@ -1,4 +1,5 @@
 import boto3, os, re
+from typing import List, Tuple
 from . import logger
 from .config_importer import (
     stop_task,
@@ -15,6 +16,9 @@ from .config_importer import (
 logs_client = boto3.client('logs')
 ecs_client = boto3.client('ecs')
 logger.info("Container initialization completed")
+
+SUCCESSFUL_STOP_REASON = "Successfully imported config to kc"
+UNSUCCESSFUL_STOP_REASON = "Import took longer than we want to wait"
 
 # This really isn't doing anything useful at all.. but i wanted to explicitly give it a type
 # since i'm considering having the generic cp invoke lambda helper consume the return value
@@ -52,48 +56,72 @@ def get_startedby_id(import_id: str):
     logger.info(f"Import id {import_id} specified; using {processed_id} as startedBy id for task")
     return processed_id
 
+def import_task_failed(task: dict) -> bool:
+    return task['lastStatus']  == 'STOPPED' and task.get('stoppedReason') != SUCCESSFUL_STOP_REASON
+
+def import_task_successfully_stopped(task: dict) -> bool:
+    return task['lastStatus']  == 'STOPPED' and task.get('stoppedReason') == SUCCESSFUL_STOP_REASON
+
+def import_task_still_starting_up(task: dict) -> bool:
+    return task['lastStatus'] != 'RUNNING'
+
+def import_task_timed_out(task: dict, log_group, log_stream) -> bool:
+    task_timed_out = taken_too_long(get_timestamp(task['startedAt']))
+    return not kc_finished_importing(logs_client, log_group, log_stream) and task_timed_out
+
+def import_task_still_importing(task: dict, log_group, log_stream) -> bool:
+    task_timed_out = taken_too_long(get_timestamp(task['startedAt']))
+    return not kc_finished_importing(logs_client, log_group, log_stream) and not task_timed_out
+
 def handler(event, context):
     cluster = os.environ['Cluster']
     task_definition = os.environ['TaskDefinition']
     task_subnets = os.environ['TaskSubnets'].split(',')
     logger.info(f"KC Config import lamba called with event: {event}")
-    
     import_id = get_startedby_id(event['ImportId'])
+    # Check if there is a running task with this import id already
     task = find_task(ecs_client, cluster, import_id)
     if not task:
+        # kickoff import task if not
         logger.info(f"Unable to find previously started task with import id: {import_id} in cluster: {cluster}... starting one now")
         task = run_task(ecs_client, cluster, task_subnets, task_definition, import_id)
-        logger.info(f"Started {task['taskArn']} in cluster {cluster}")
-        return CodePipelineHelperResponse.in_progress(f'{task["taskArn"]} has been started')
+        message = f"Started {task['taskArn']} in cluster {cluster}"
+        logger.info(message)
+        return CodePipelineHelperResponse.in_progress(message)
     
+    # if a task was found lets get more info from it to verify if it is 
     task_status = task['lastStatus']
     task_arn = task['taskArn']
     logger.info(f"Found task {task_arn} previously started with import id {import_id} in cluster {cluster}")
     log_group, log_stream = get_task_logs_location(ecs_client, task['taskDefinitionArn'], task_arn)
     
-    successful_stop_reason = "Successfully imported config to kc"
-
-    if task_status  == 'STOPPED' and task.get('stoppedReason') != successful_stop_reason:
+    if import_task_failed(task):
         message = f"{task_arn} was found in unexpected stop state. Check {log_group}/{log_stream} for logs"
         logger.warning(message)
         return CodePipelineHelperResponse.failed(message)
-    elif task_status  == 'STOPPED' and task.get('stoppedReason') == successful_stop_reason:
+    elif import_task_successfully_stopped(task):
         message = f"{task_arn} was stopped after successful import. Returning success since this is most likely a retry that should no-op"
         logger.warning(message)
         return CodePipelineHelperResponse.succeeded(message)
-    elif task_status != 'RUNNING':
-        message = f"{task_arn} status {task_status} not in stable state; checking again later"
+    elif import_task_still_starting_up(task):
+        message = f"{task_arn} status {task_status} not in stable state; check again later..."
         logger.info(message)
         return CodePipelineHelperResponse.in_progress(message)
-    # since we know the task is RUNNING it should be safe to snag the startedAt prop now
-    start_time =  get_timestamp(task['startedAt'])
-    task_timed_out = taken_too_long(start_time)
-    if not kc_finished_importing(logs_client, log_group, log_stream) and task_timed_out:
-        message = f"KC Import config task ({task_arn}) still not done starting and we are done waiting"
+
+    
+    if import_task_still_importing(task, log_group, log_stream):
+        message = f"KC Import config task ({task_arn}) still running through startup/import process; check again later..."
+        logger.info(message)
+        return CodePipelineHelperResponse.in_progress(message)
+    elif import_task_timed_out(task, log_group, log_stream):
+        message = f"KC Import config task ({task_arn}) still not done importing and we are done waiting"
         logger.warning(message)
-        stop_task(ecs_client, cluster, task_arn, "Import took longer than we want to wait")
+        stop_task(ecs_client, cluster, task_arn, UNSUCCESSFUL_STOP_REASON)
         return CodePipelineHelperResponse.failed(message)
+
+    # if here then the task is running as expected and has completed the import process
+    # so it needs to get stopped
     message = f"{task_arn} has successfully finished importing config; stopping it..."
     logger.info(message)
-    stop_task(ecs_client, cluster, task_arn, successful_stop_reason )
+    stop_task(ecs_client, cluster, task_arn, SUCCESSFUL_STOP_REASON )
     return CodePipelineHelperResponse.succeeded(message, OutputVariables={'TaskArn': task_arn})
