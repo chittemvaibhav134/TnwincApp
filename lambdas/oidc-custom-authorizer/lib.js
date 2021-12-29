df4dd8f53b3497939cd184276cc1d5f2550245c9
Jonathan Jensen <jjensen@navexglobal.com>
2021-12-29 09:42:34 -0800

    Remove redundant check

--- lambdas/oidc-custom-authorizer/lib.js
+++ lambdas/oidc-custom-authorizer/lib.js
@@ -18,10 +18,6 @@ const getPolicyDocument = (effect, resource) => {
 
 // extract and return the Bearer Token from the Lambda event parameters
 const getToken = (params) => {
-    if (!params.type) {
-        throw new Error('Expected "event.type" parameter to have value "TOKEN" or "REQUEST"');
-    }
-
     const eventType = params.type;
 
     if (eventType === 'TOKEN') {
