ac75207b95ff05f5fcc7740d0bd3023d611dd4fa
Jonathan Jensen <jjensen@navexglobal.com>
2022-01-10 16:06:21 -0800

    Add authorizer to both endpoints

--- lambdas/oidc-custom-authorizer/index.js
+++ lambdas/oidc-custom-authorizer/index.js
@@ -4,6 +4,8 @@ let data;
 // Lambda function index.handler - thin wrapper around lib.authenticate
 module.exports.handler = async (event, context) => {
   try {
+    console.log(event)
+    console.log(context)
     data = await lib.authenticate(event);
   }
   catch (err) {
